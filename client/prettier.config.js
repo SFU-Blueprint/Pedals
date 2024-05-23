b92fec0091defc51d46fb3e6706e9b150ca6c6b5
dangminhduc1101 <ddang@vassar.edu>
2024-05-23 14:58:38 -0700

    Fixes occlusion between different eslint plugins

--- client/prettier.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("prettier").Config} */
-module.exports = {
-  trailingComma: "none",
-  tabWidth: 2,
-  semi: true,
-  singleQuote: false,
-  plugins: ["prettier-plugin-tailwindcss"]
-};
