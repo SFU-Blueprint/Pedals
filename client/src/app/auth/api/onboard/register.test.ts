839c660a3748a09c29715f1b3abd53e3e1a7d237
Quang Vinh Dang <dqvinh101@gmail.com>
2024-06-08 23:30:58 -0700

    test: Add test for unique email

--- client/src/app/auth/api/onboard/register.test.ts
+++ client/src/app/auth/api/onboard/register.test.ts
@@ -1,8 +1,23 @@
-import registration  from "./register";
+import {registration, findDuplicateUserEmail}  from "./register";
+
+jest.mock('./register', () => {
+	return {
+		...jest.requireActual('./register'),
+		findDuplicateUserEmail: jest.fn()
+	}
+})
 
 describe('Testing Registration on volunteer ', () => {
-    test('Upon successful registration, it should return 200 status', () => {
-        expect(1).toBe(1);
-    });
+	test("If there is a duplicate in the email, it should return an errors", async () => {
+		const {findDuplicateUserEmail} = require("./register");
+
+		findDuplicateUserEmail.mockImplementation(() => true);
+		const result = await registration(
+			"Test 1",
+			"test@gmail.com",
+			true
+		);
+		expect(result.status).toBe("error");
+	})
 });
 
--- client/src/app/auth/api/onboard/register.ts
+++ client/src/app/auth/api/onboard/register.ts
@@ -1,6 +1,48 @@
-export default async function registration(
+import { warn } from "console"
+
+export function findDuplicateUserEmail(
+	email: string
+): boolean {
+	return true
+}
+
+interface RegistrationResponse {
+	status: string,
+	error?: {
+		code: number,
+		message: string
+	}
+	data?: {
+		user: {
+			id: number
+		}
+	}
+}
+
+export async function registration(
 	name: string,
-	contactInfo: string,
-	isOver18: bool
-){
+	email: string,
+	isOver18: boolean
+): Promise<RegistrationResponse> {
+	//Verify user information
+	if ( findDuplicateUserEmail (email) ) {
+		return {
+			status: "error",
+			error: {
+				code: 400,
+				message: "Duplicate email"
+			}
+		}
+	}
+	//Connects to the database
+	//
+	//If the database failed return 400, else 200
+	
+	return {
+		status: "error",
+		error: {
+			code: 400,
+			message: "Duplicate email"
+		}
+	}
 }
