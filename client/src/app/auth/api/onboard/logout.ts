57810bdd9987433c780654a7925c35cc2df58f67
ayana <ayanahye2@gmail.com>
2024-06-12 18:34:50 -0700

    no lint or format errors

--- client/src/app/auth/api/onboard/logout.ts
+++ client/src/app/auth/api/onboard/logout.ts
@@ -1,8 +1,8 @@
 interface Response {
-    status(code: number): Response;
-    json(data:any): Response
+  status(code: number): Response;
+  json(data: any): Response;
 }
 
 export default async function handler(req: any, res: Response) {
-
+  res.status(200).json({ message: "Handler executed" });
 }
