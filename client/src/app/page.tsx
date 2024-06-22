a73dcbb05e9dbe87cc2210463c94bdd45f83fdc9
Quang Vinh Dang <dqvinh101@gmail.com>
2024-06-22 14:42:29 -0700

    Update UI

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -11,9 +11,18 @@ export default function Home() {
   return (
 	<nav className="bg-[#E9E9E9]">
 		
-		<div className="flex flex-col">
-			<h1>{data[0]}, {data[1]} {data[2]}</h1>
-			<h1 className="font-supplyMono">{data[3]} {data[4]}</h1>
+		<div className="flex flex-row justify-around">
+			<div className="flex flex-col">
+				<h1 className="font-supplyMono">{data[0]}, {data[1]} {data[2]}</h1>
+				<h1 className="font-supplyMono text-[64px]">{data[3]} {data[4]}</h1>
+			</div>
+			<div>
+				<ul className="flex flex-row">
+					<li>Check-in</li>
+					<li>Register</li>
+					<li>Manage</li>
+				</ul>
+			</div>
 		</div>
 	</nav>
   );
