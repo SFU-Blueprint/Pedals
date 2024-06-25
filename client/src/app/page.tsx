afc5a46f9e28086888b7c6a57cd0a1c82f3470e2
Quang Vinh Dang <dqvinh101@gmail.com>
2024-06-24 20:10:44 -0700

    fix: Fix format error

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -88,8 +88,12 @@ export default function Home() {
             {" "}
             <h1 className="mb-2 text-[18px]">ShiftName</h1>
             <div className="flex flex-row">
-              <button type = "button" className="mr-10 bg-white px-3 py-1">PFTP</button>
-              <button type = "button" className="mr-10 bg-white px-3 py-1">WTQ</button>
+              <button type="button" className="mr-10 bg-white px-3 py-1">
+                PFTP
+              </button>
+              <button type="button" className="mr-10 bg-white px-3 py-1">
+                WTQ
+              </button>
               <div className="mr-10 flex justify-center bg-white px-3 py-1 align-middle">
                 <select className="bg-white">
                   <option> Option 1 </option>
