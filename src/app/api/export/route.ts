d2320c2aa086d6a002bada8010ebd99dd85ee8a7
Quang Vinh Dang <dqvinh101@gmail.com>
2024-07-15 22:13:41 -0700

    feature: Adding export api route

--- src/app/api/export/route.ts
+++ src/app/api/export/route.ts
@@ -1,10 +1,58 @@
-import { NextResponse, NextRequest } from "next/server";
+import { createClient } from '@supabase/supabase-js';
+import { NextResponse } from 'next/server';
 
 export const GET = async () => {
-  // To be implement, not sure how we will tackle this
+  const supabaseUrl = process.env.NEXT_APP_SUPABASE_URL as string;
+  const key = process.env.SUPABASE_KEY as string;
 
-  return NextResponse.json({
-    message: "Success",
-    status: 200
-  });
+  try {
+    const supabase = createClient(supabaseUrl, key);
+    const { data, error } = await supabase
+      .from("access_codes")
+      .select("*");
+
+    if (error) {
+      return NextResponse.json({ message: error.message }, { status: 500 });
+    }
+
+    if (!data) {
+      return NextResponse.json({ message: "No data found" }, { status: 404 });
+    }
+
+    // Convert data to CSV
+    const csv = convertToCSV(data);
+
+    // // Set headers for CSV download
+    const headers = new Headers();
+    headers.append('Content-Type', 'text/csv');
+    headers.append('Content-Disposition', 'attachment; filename=volunteers.csv');
+
+    // Return the CSV data
+    return new NextResponse(csv, {
+      status: 200,
+      headers: headers,
+    });
+
+    // return NextResponse.json({ message: "Success"}, { status: 200});
+
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json({ message: "An unexpected error occurred" }, { status: 500 });
+  }
 };
+
+function convertToCSV(arr: any[]): string {
+  if (arr.length === 0) return '';
+  
+  const headers = Object.keys(arr[0]);
+  const csvRows = [
+    headers.join(','), // CSV header row
+    ...arr.map(row => 
+      headers.map(fieldName => 
+        JSON.stringify(row[fieldName] ?? '')
+      ).join(',')
+    )
+  ];
+  
+  return csvRows.join('\n');
+}
