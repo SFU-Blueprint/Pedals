65ddbcfc6e0d6b18fd05be99d4aa90a6d382cb44
ayana <ayanahye2@gmail.com>
2024-07-21 14:13:28 -0700

    Fix linting errors, modified eslintrc

--- src/app/api/people/[id]/route.ts
+++ src/app/api/people/[id]/route.ts
@@ -1,32 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-// https://maryetokwudo.hashnode.dev/nextjs-13-route-handlers-with-typescript
+// DELERE /api/people/:id
 /* eslint-enable import/prefer-default-export */
-export const GET = async () => {
-  const supabaseUrl = process.env.NEXT_APP_SUPABASE_URL as string;
-  const key = process.env.SUPABASE_KEY as string;
-
-  try {
-    const supabase = createClient(supabaseUrl, key);
-    const { data, error } = await supabase.from("users").select("name");
-    if (error) {
-      return NextResponse.json({ message: error });
-    }
-
-    return NextResponse.json(data, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ message: error }, { status: 500 });
-  }
-};
-
-// not sure how to do query parameters here...
 export const DELETE = async (request: NextRequest) => {
   const supabaseUrl = process.env.NEXT_APP_SUPABASE_URL as string;
   const key = process.env.SUPABASE_KEY as string;
 
   try {
-    const { id } = await request.json();
+    const { pathname } = new URL(request.url);
+    const id = pathname.split("/").pop();
+
     if (!id) {
       return NextResponse.json({
         message: "Please provide an id"
@@ -47,5 +31,3 @@ export const DELETE = async (request: NextRequest) => {
     return NextResponse.json({ message: error }, { status: 500 });
   }
 };
-
-// sid, created_at, name, dob, pronoun, email, phone
