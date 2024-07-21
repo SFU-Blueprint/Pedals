95e50a4250df88baf64b7c194fb5a5f1adacc0cf
Terry <terrykwok147@gmail.com>
2024-07-21 14:36:24 -0700

    minor fixes

--- src/app/api/validate-access-code/route.ts
+++ src/app/api/validate-access-code/route.ts
@@ -8,7 +8,6 @@ export async function POST(req: Request) {
 
   const body = await req.json();
   const { accessCodeToCheck } = body;
-
   if (!accessCodeToCheck) {
     return NextResponse.json(
       { error: "Missing required field" },
