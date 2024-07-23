d7314b4144cc35c6b248b891b2d27e53378c1601
ayana <ayanahye2@gmail.com>
2024-07-22 18:35:25 -0700

    Fix DELETE endpoint for people

--- src/app/api/people/route.ts
+++ src/app/api/people/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
 // https://maryetokwudo.hashnode.dev/nextjs-13-route-handlers-with-typescript
@@ -15,34 +15,6 @@ export const GET = async () => {
     }
 
     return NextResponse.json(data, { status: 200 });
-  } catch (error) {
-    return NextResponse.json({ message: error }, { status: 500 });
-  }
-};
-
-// not sure how to do query parameters here...
-export const DELETE = async (request: NextRequest) => {
-  const supabaseUrl = process.env.NEXT_APP_SUPABASE_URL as string;
-  const key = process.env.SUPABASE_KEY as string;
-
-  try {
-    const { id } = await request.json();
-    if (!id) {
-      return NextResponse.json({
-        message: "Please provide an id"
-      });
-    }
-    const supabase = createClient(supabaseUrl, key);
-    const { error } = await supabase.from("users").delete().eq("sid", id);
-
-    if (error) {
-      return NextResponse.json({ message: error });
-    }
-
-    return NextResponse.json(
-      { message: "User deleted successfully" },
-      { status: 200 }
-    );
   } catch (error) {
     return NextResponse.json({ message: error }, { status: 500 });
   }
