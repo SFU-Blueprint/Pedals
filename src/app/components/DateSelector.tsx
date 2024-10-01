451c917155314e7ffbfed40fa44810a2b3ce1c20
terry9041 <terrykwok147@gmail.com>
2024-10-01 12:18:25 -0700

    fix: added a custom css for datepicker to make it look like the Figma design

--- src/app/components/DateSelector.tsx
+++ src/app/components/DateSelector.tsx
@@ -1,8 +1,23 @@
 import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import "./custom-datepicker.css";
 
 type DateSelectorProps = React.ComponentProps<typeof DatePicker>;
 
 export default function DateSelector(props: DateSelectorProps) {
-  return <DatePicker dateFormat="MMM d, yyyy" {...props} />;
+  return (
+    <DatePicker
+      showYearDropdown
+      dateFormatCalendar="MMMM"
+      yearDropdownItemNumber={130}
+      scrollableYearDropdown
+      {...props}
+    />
+    // <DatePicker
+    //   showIcon
+    //   toggleCalendarOnIconClick
+    //   dateFormat="MMM d, yyyy"
+    //   className="uppercase"
+    //   {...props}
+    // />
+  );
 }
